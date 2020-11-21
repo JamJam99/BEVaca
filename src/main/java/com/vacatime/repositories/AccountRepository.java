5df6a752e5bcd1975d297f20b6ac3969112d051f
JamJam99 <jamalabdau93@gmail.com>
2020-11-21 23:46:57 +0700

    push

--- src/main/java/com/vacatime/repositories/AccountRepository.java
+++ src/main/java/com/vacatime/repositories/AccountRepository.java
@@ -1,5 +1,11 @@
 package com.vacatime.repositories;
 
-public interface AccountRepository {
+import com.vacatime.models.Account;
+import com.vacatime.models.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository <Account, Long> {
 
 }
